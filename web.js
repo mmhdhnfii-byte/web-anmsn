ada63423a2b03c73a0f9b71fe62305fb3c2e3b8a
mmhdhnfii-byte <mmhdhnfii@gmail.com>
2026-01-29 07:33:07 +0700

    Update web.js

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-const audio = document.getElementById("say-yellow.mp3");
+const audio = document.getElementById("audio");
 const btn = document.getElementById("playBtn");
 const lyricsEl = document.getElementById("lyrics");
 
@@ -24,5 +24,6 @@ setInterval(() => {
   lyricsEl.textContent = lyrics[i];
   i = (i + 1) % lyrics.length;
 }, 3000);
+
 
 
